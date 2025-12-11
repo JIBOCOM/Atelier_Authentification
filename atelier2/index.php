047d0a3dabd88a058815d5bd5f906b726aec4a94
BAKIYAMA <162821018+JIBOCOM@users.noreply.github.com>
2025-12-11 12:04:09 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,26 +1,25 @@
 <?php
-// Démarre la session
+// Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
 
-// Vérifier si l'utilisateur est déjà connecté
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
+// Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
+// Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
+// Dans le cas contraire il devra s'identifier.
+if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === '12345') {
+    header('Location: page_admin.php');
     exit();
 }
 
-// Gérer le formulaire de connexion
+// Gérer la soumission du formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Vérification simple des identifiants (à améliorer avec une base de données)
+    // Vérification simple du username et de son password.
+    // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        // Stocker les informations utilisateur dans la session
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $username;
-
-        // Rediriger vers la page protégée
-        header('Location: page_admin.php');
+        setcookie('authToken', '12345', time() + 3600, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
@@ -32,11 +31,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
 </head>
 <body>
-    <h1>Atelier authentification par Session</h1>
-    <h3>La page <a href="page_admin.php">page_admin.php</a> de cet atelier 3 est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
+    <h1>Atelier authentification par Cookie</h1>
+    <h3>La page <a href="page_admin.php">page_admin.php</a> est inaccéssible tant que vous ne vous serez pas connecté avec le login 'admin' et mot de passe 'secret'</h3>
     <form method="POST" action="">
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" required>
